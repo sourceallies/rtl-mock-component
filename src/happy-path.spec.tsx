303bc71ae9a88325b239636d8fbd2344c1636f4e
Paul Rowe <prowe@sourceallies.com>
2022-01-20 20:20:59 -0600

    searching within an element

--- src/happy-path.spec.tsx
+++ src/happy-path.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { buildQueries, getByRole, render, screen, within } from '@testing-library/react';
 import React, { useEffect, useState } from 'react';
 import { setupMockComponent, getByMockComponent } from './index';
 import { findAllByMockComponent, findByMockComponent, getAllByMockComponent, queryAllByMockComponent, queryByMockComponent } from './queries';
@@ -64,7 +64,7 @@ it('should find via getAllByMockComponent', () => {
     expect(all).toHaveLength(2);
     expect(all[0]).toBeInTheDocument();
     expect(all[1]).toBeInTheDocument();
-    expect(all[0]).not.toBe(all[2]);
+    expect(all[0]).not.toBe(all[1]);
 });
 
 it('should find via queryByMockComponent', () => {
@@ -121,4 +121,115 @@ it('should find via findAllByMockComponent', async () => {
     expect(all[0]).toBeInTheDocument();
     expect(all[1]).toBeInTheDocument();
     expect(all[0]).not.toBe(all[2]);
+});
+
+describe('Searching within a containing element', () => {
+    it('should find within an element via getByMockComponent', () => {
+        render(
+            <ul>{['A', 'B'].map(value => <li key={value}><MockedComponent value={value} /></li>)}</ul>
+        );
+
+        const container = screen.getAllByRole('listitem')[0];
+        expect(getByMockComponent(MockedComponent, {container}).props.value).toEqual('A');
+    });
+
+    it('should find within an element via getAllByMockComponent', () => {
+        render(
+            <ul>{['A', 'B'].map(value => 
+                <li key={value}>
+                    <MockedComponent value={value} />
+                    <MockedComponent value={value} />
+                </li>)}
+            </ul>
+        );
+
+        const container = screen.getAllByRole('listitem')[0];
+        const all = getAllByMockComponent(MockedComponent, {container});
+        expect(all).toHaveLength(2);
+        expect(all[0].props.value).toEqual('A');
+        expect(all[1].props.value).toEqual('A');
+        expect(all[0]).not.toBe(all[1]);
+    });
+
+    it('should find within an element via queryByMockComponent', () => {
+        render(
+            <ul>{['A', 'B'].map(value => 
+                <li key={value}>
+                    <MockedComponent value={value} />
+                </li>)}
+            </ul>
+        );
+
+        const container = screen.getAllByRole('listitem')[0];
+        expect(queryByMockComponent(MockedComponent, {container})?.props.value).toEqual('A');
+    });
+
+    it('should find within an element via queryAllByMockComponent', () => {
+        render(
+            <ul>{['A', 'B'].map(value => 
+                <li key={value}>
+                    <TestedComponent value={value} />
+                    <TestedComponent value={value} />
+                </li>)}
+            </ul>
+        );
+
+        const container = screen.getAllByRole('listitem')[0];
+        const all = queryAllByMockComponent(MockedComponent, {container});
+        expect(all).toHaveLength(2);
+        expect(all[0]).toBeInTheDocument();
+        expect(all[1]).toBeInTheDocument();
+        expect(all[0]).not.toBe(all[1]);
+    });
+
+    it('should find within an element via findByMockComponent', async () => {
+        const TimedFunction = () => {
+            const [visible, setVisible] = useState(false);
+            useEffect(() => {
+                const handle = setTimeout(() => setVisible(true), 10);
+                return () => clearTimeout(handle);
+            }, []);
+            return <div>{visible && <MockedComponent value='' />}</div>;
+        };
+        render(
+            <ul>{['A', 'B'].map(value => 
+                <li key={value}>
+                    <TimedFunction />
+                </li>)}
+            </ul>
+        );
+
+        const container = screen.getAllByRole('listitem')[0];
+        expect(await findByMockComponent(MockedComponent, {container})).toBeInTheDocument();
+    });
+
+    it('should find within an element via findAllByMockComponent', async () => {
+        const TimedFunction = () => {
+            const [visible, setVisible] = useState(false);
+            useEffect(() => {
+                const handle = setTimeout(() => setVisible(true), 10);
+                return () => clearTimeout(handle);
+            }, []);
+            return (
+                <div>
+                    {visible && <span><MockedComponent value='' /><MockedComponent value='' /></span>}
+                </div>
+            );
+        };
+        render(
+            <ul>{['A', 'B'].map(value => 
+                <li key={value}>
+                    <TimedFunction />
+                    <TimedFunction />
+                </li>)}
+            </ul>
+        );
+
+        const container = screen.getAllByRole('listitem')[0];
+        const all = await findAllByMockComponent(MockedComponent, {container});
+        expect(all).toHaveLength(2);
+        expect(all[0]).toBeInTheDocument();
+        expect(all[1]).toBeInTheDocument();
+        expect(all[0]).not.toBe(all[1]);
+    });
 });
