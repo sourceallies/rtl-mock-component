e066a07472fea41a3ec00034d577d3b925712106
Paul Rowe <prowe@sourceallies.com>
2022-06-19 12:46:34 -0500

    removing jest runtime dep

--- src/bad-setup.spec.tsx
+++ src/bad-setup.spec.tsx
@@ -6,6 +6,6 @@ it('should throw an exception if the component is not a mock', () => {
     try {
         setupMockComponent(MockedComponent);
     } catch (err: any) {
-        expect(err.message).toEqual("MockedComponent cannot be setup because it is not a Jest mock. Call \"jest.mock('path/to/component')\" first");
+        expect(err.message).toEqual("MockedComponent cannot be setup because it is not a compatable mock. Call \"jest.mock('path/to/component')\" first");
     }
 });
